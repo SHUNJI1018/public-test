4079cac371b3ef59e7a1a4a2dafa8e836c899012
SHUNJI1018 <hide971gg@gmail.com>
2021-06-28 03:19:00 +0000

    java

--- java/main/Main.java
+++ java/main/Main.java
@@ -6,5 +6,12 @@ public class Main {
     b = a + 5;
     System.out.println(a);
     System.out.println(b);
+    System.out.println("私の好きな記号は二重引用符（\"）です");
+    
+    double c = 9.0;
+    System.out.println(c / 2);
+    
+    b++;
+    System.out.println(b);
   }
 }
