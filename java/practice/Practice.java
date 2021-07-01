f27778c3cafe7ccdcfa4253a439052b323fcf5ae
SHUNJI1018 <hide971gg@gmail.com>
2021-07-01 02:45:04 +0000

    java

--- java/practice/Practice.java
+++ java/practice/Practice.java
@@ -4,5 +4,15 @@ public class Practice {
     double d = f;
     System.out.println(f);
     System.out.println(d);
+    
+    double e = 8.5 / 2;
+    long g = 5 + 2L;
+    System.out.println(e);
+    System.out.println(g);
+    
+    int a = 5;
+    int b = 3;
+    int m = Math.max(a, b);
+    System.out.println("比較実験:" + a + "と" + b + "とで大きい方は・・・" + m);
   }
 }
